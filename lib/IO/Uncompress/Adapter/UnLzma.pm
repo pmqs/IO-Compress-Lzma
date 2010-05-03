cb9ca30ebf18348c3590de6e48fefe027ea5804a
pmqs <pmqs@cpan.org>
2010-05-03 10:10:10 +0100

    import IO-Compress-Lzma 2.028
    git-cpan-module:   IO-Compress-Lzma
    git-cpan-version:  2.028
    git-cpan-authorid: pmqs

--- lib/IO/Uncompress/Adapter/UnLzma.pm
+++ lib/IO/Uncompress/Adapter/UnLzma.pm
@@ -4,12 +4,12 @@ use strict;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common 2.027 qw(:Status);
+use IO::Compress::Base::Common 2.028 qw(:Status);
 
-use Compress::Raw::Lzma 2.027 ;
+use Compress::Raw::Lzma 2.028 ;
 
 our ($VERSION, @ISA);
-$VERSION = '2.027';
+$VERSION = '2.028';
 
 #@ISA = qw( Compress::Raw::UnLzma );
 

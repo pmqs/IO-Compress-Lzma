92737696ede01ef6d44a51b0244eeca4354e1213
pmqs <pmqs@cpan.org>
2010-05-08 14:02:36 +0100

    import IO-Compress-Lzma 2.029
    git-cpan-module:   IO-Compress-Lzma
    git-cpan-version:  2.029
    git-cpan-authorid: pmqs

--- lib/IO/Uncompress/Adapter/UnLzma.pm
+++ lib/IO/Uncompress/Adapter/UnLzma.pm
@@ -4,12 +4,12 @@ use strict;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common 2.028 qw(:Status);
+use IO::Compress::Base::Common 2.029 qw(:Status);
 
-use Compress::Raw::Lzma 2.028 ;
+use Compress::Raw::Lzma 2.029 ;
 
 our ($VERSION, @ISA);
-$VERSION = '2.028';
+$VERSION = '2.029';
 
 #@ISA = qw( Compress::Raw::UnLzma );
 
--- lib/IO/Uncompress/UnLzma.pm
+++ lib/IO/Uncompress/UnLzma.pm
@@ -4,15 +4,15 @@ use strict ;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common 2.028 qw(:Status createSelfTiedObject);
+use IO::Compress::Base::Common 2.029 qw(:Status createSelfTiedObject);
 
-use IO::Uncompress::Base 2.028 ;
+use IO::Uncompress::Base 2.029 ;
 use IO::Uncompress::Adapter::UnLzma 2.006 ;
 
 require Exporter ;
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $UnLzmaError);
 
-$VERSION = '2.028';
+$VERSION = '2.029';
 $UnLzmaError = '';
 
 @ISA    = qw( Exporter IO::Uncompress::Base );
@@ -40,7 +40,7 @@ sub getExtraParams
 {
     my $self = shift ;
 
-    use IO::Compress::Base::Common 2.028 qw(:Parse);
+    use IO::Compress::Base::Common 2.029 qw(:Parse);
     
     return (
         #'Verbosity'     => [1, 1, Parse_boolean,   0],

fd4f0585fc6d33fd3973ce8c63bd1067f96429e1
pmqs <pmqs@cpan.org>
2021-01-07 12:50:07 +0000

    2.100

--- lib/IO/Compress/Lzma.pm
+++ lib/IO/Compress/Lzma.pm
@@ -5,16 +5,16 @@ use warnings;
 use bytes;
 require Exporter ;
 
-use IO::Compress::Base 2.096 ;
+use IO::Compress::Base 2.100 ;
 
-use IO::Compress::Base::Common  2.096 qw(createSelfTiedObject);
-use IO::Compress::Adapter::Lzma 2.096 ;
+use IO::Compress::Base::Common  2.100 qw(createSelfTiedObject);
+use IO::Compress::Adapter::Lzma 2.100 ;
 
 
 
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $LzmaError);
 
-$VERSION = '2.096';
+$VERSION = '2.100';
 $LzmaError = '';
 
 @ISA    = qw(IO::Compress::Base Exporter);
@@ -771,7 +771,7 @@ See the Changes file.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2005-2020 Paul Marquess. All rights reserved.
+Copyright (c) 2005-2021 Paul Marquess. All rights reserved.
 
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
--- lib/IO/Uncompress/UnLzma.pm
+++ lib/IO/Uncompress/UnLzma.pm
@@ -4,15 +4,15 @@ use strict ;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common 2.096 qw(:Status createSelfTiedObject);
+use IO::Compress::Base::Common 2.100 qw(:Status createSelfTiedObject);
 
-use IO::Uncompress::Base 2.096 ;
-use IO::Uncompress::Adapter::UnLzma 2.096 ;
+use IO::Uncompress::Base 2.100 ;
+use IO::Uncompress::Adapter::UnLzma 2.100 ;
 
 require Exporter ;
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $UnLzmaError);
 
-$VERSION = '2.096';
+$VERSION = '2.100';
 $UnLzmaError = '';
 
 @ISA    = qw( IO::Uncompress::Base Exporter );
@@ -941,7 +941,7 @@ See the Changes file.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2005-2020 Paul Marquess. All rights reserved.
+Copyright (c) 2005-2021 Paul Marquess. All rights reserved.
 
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
